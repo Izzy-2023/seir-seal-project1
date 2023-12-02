a30bc6a500fd83e3cd4e279bdf85b7c5a88304fa
izzy-2023 <izzyzinxhirija@gmail.com>
2023-12-02 00:47:50 -0500

    test out another api call

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // *************************************
 // 
 // *************************************
-
-$.ajax("https://api.nasa.gov/planetary/apod?api_key=sByYd8sUGG38uhb6ZjJf50d45fowIfERgprTWT0D")
+// API Key: sByYd8sUGG38uhb6ZjJf50d45fowIfERgprTWT0D
+$.ajax("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=sByYd8sUGG38uhb6ZjJf50d45fowIfERgprTWT0D")
 .then((data) => {
     console.log(data)
-    console.log(data.date)
+    console.log(data.photos[0].id)
 })
